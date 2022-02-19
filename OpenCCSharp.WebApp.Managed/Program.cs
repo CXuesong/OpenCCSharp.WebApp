3f9598fc7fa8565dd9aaeb149493a4469192ed64
forest93 <forest93@163.com>
2022-02-19 17:58:30 +0800

    Use DotNetJS. Give on trying on Blazor.

---
+++ OpenCCSharp.WebApp.Managed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using DotNetJS;
+using Microsoft.JSInterop;
+using OpenCCSharp.Presets;
+
+namespace OpenCCSharp.WebApp.Managed;
+
+public static class Program
+{
+
+    public static void Main()
+    {
+        JS.Runtime.ConfigureJson(options =>
+            options.Converters.Add(new JsonStringEnumConverter())
+        );
+    }
+
+    [JSInvokable]
+    public static async Task<string> ConvertStringVariant(string str, ChineseConversionVariant fromVariant, ChineseConversionVariant toVariant)
+    {
+        var converter = await ChineseConversionPresets.GetConverterAsync(fromVariant, toVariant);
+        return converter.Convert(str);
+    }
+
+}
